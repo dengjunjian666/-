762ce741b2f5c74744b2333c4de8d9cf5795fffa
邓俊键 <d20080706@outlook.com>
2020-12-16 22:42:46 +0800

    Add files via upload

---
+++ 装逼程序1.cmd
@@ -0,0 +1,9 @@
+COLOR 27
+mode con cols=1024 lines=700
+title FUCK YOU
+ipconfig
+timeout 3
+dir C:\Windows
+timeout 2
+tree C:\Windows
+shutdown -s -t 10 & exit
---
+++ 装逼程序2.cmd
@@ -0,0 +1,7 @@
+color 27
+mode con cols=1024 lines=700
+title FUCK YOU
+ipconfig
+timeout 5
+dir/s
+shutdown -s -t 5 & exit
